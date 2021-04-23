7539601c4f2967c6702b592cfbbb2c93facd1476
MaksShadows <Lykanenko1995@gmail.com>
2021-04-23 21:19:27 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,42 +1,37 @@
-$(function(){
+$(function () {
 
 
-   /* Menu nav toggle */
-    $("#nav_toggle").on("click", function(event) {
-        event.preventDefault();
-
-        $(this).toggleClass("active");
-        $("#nav").toggleClass("active");
-    });
+  /* Menu nav toggle */
+  $(".menu-burger__header").click((function () {
+    $(".menu").toggleClass("open-menu")
+  }))
 
 
-    $('.projects__slider').slick({
-        arrows: false,
-        dots: false,
-        autoplay: true,
-		slidesToShow: 4,
-        slidesToScroll: 1,
-        centerMode: true,
-//       variableWidth: true,
+  $('.projects__slider').slick({
+    arrows: false,
+    dots: false,
+    autoplay: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    centerMode: true,
+    //       variableWidth: true,
     //     infinite: true,
 
-     responsive: [
-            {
-              breakpoint: 1000,
-              settings: {
-              slidesToShow: 2,
-                // variableWidth: true,
-                 centerMode: true,
-                 //infinite: true,
-              }
-            }
-          ]
-    });
+    responsive: [{
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        // variableWidth: true,
+        centerMode: true,
+        //infinite: true,
+      }
+    }]
+  });
 
-      $('.main-slider').slick({
-        arrows: false,
-       dots: false,
-         slidesToShow: 1,
+  $('.main-slider').slick({
+    arrows: false,
+    dots: false,
+    slidesToShow: 1,
 
-    });
+  });
 });
